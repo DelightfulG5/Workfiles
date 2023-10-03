4319f6a4b237ec41405510252afddacf15c5e472
Gabriel <gymlyn2004@gmail.com>
2023-10-03 11:01:14 +0200

    added backticks

--- JS-Work/index.js
+++ JS-Work/index.js
@@ -14,14 +14,14 @@ studentLastName = "Newman";
 
 //3. Print in console the following statement: "<value of studentFirstName> <value of studentLastName> has an average grade of <value of average>"
 //ex. Jane Doe has an average grade of 100.
-console.log('${studentFirstName} ${studentLastName} has an average grade of ${average}.');
+console.log(`${studentFirstName} ${studentLastName} has an average grade of ${average}.`);
 
 //4. Write code that would answer this question: Did the student pass?
 let didPass = average >= passingGrade;
 
 if (didPass) {
-    console.log('${studentFirstName} ${studentLastName} passed.');
+    console.log(`${studentFirstName} ${studentLastName} passed.`);
 } 
 else {
-    console.log('${studentFirstName} ${studentLastName} did not pass.');
+    console.log(`${studentFirstName} ${studentLastName} did not pass.`);
 }
